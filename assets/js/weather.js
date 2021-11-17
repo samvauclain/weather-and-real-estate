09709e3c4a2b71fcf95b365361a97504bc4f40f6
Cydica <85818465+ashsaidhi99@users.noreply.github.com>
2021-11-17 12:40:54 -0700

    More JS/Index

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -8,7 +8,7 @@ searchbox.addEventListener('keypress', setQuery);
 
 function setQuery(evt) {
     if (evt.keyCode == 13) {
-        getResults(searchbox.value);
+        getResults(input.value);
     }
 }
 
@@ -22,4 +22,25 @@ function getResults (query) {
 function displayResults (weather) {
     let temp = document.querySelector('.current .title is-1');
     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp)}°c / ${Math.round(weather.main.temp_max)}°c`;
+
+    let city = document.querySelector('.location .city');
+    city.innerText = `${Weather.name}, ${weather.sys.country}`;
+}
+
+function dataBuilder (d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getdate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
 }
